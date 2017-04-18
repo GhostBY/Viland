66f8ab866a46fe4c6f645f274667b1973d5cee4f
GhostBY <alexandrlukashov@outlook.com>
2017-04-18 20:42:36 +0300

    complete DAL for Article

---
+++ Viland.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Viland.DAL.Entities;
+
+namespace Viland.DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Article> Articles { get; }
+        void Save();
+    }
+}
