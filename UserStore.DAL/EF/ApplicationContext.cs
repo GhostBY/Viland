b51f693917399b3421c02111184b7b9b8c53561f
GhostBY <alexandrlukashov@outlook.com>
2017-04-30 21:19:32 +0300

    reload project with Identity

---
+++ UserStore.DAL/EF/ApplicationContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using UserStore.DAL.Entities;
+
+namespace UserStore.DAL.EF
+{
+    public class ApplicationContext: IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationContext(string conectionString) : base(conectionString) { }
+        public ApplicationContext()
+            : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<ClientProfile> ClientProfiles { get; set; }
+        public void Commit()
+        {
+            SaveChanges();
+        }
+
+        public void CommitAsync()
+        {
+            SaveChangesAsync();
+        }
+    }
+    public class UserStoreDbInitializer : DropCreateDatabaseIfModelChanges<ApplicationContext>
+    {
+        protected override void Seed(ApplicationContext db)
+        {
+
+            db.SaveChanges();
+        }
+    }
+}
